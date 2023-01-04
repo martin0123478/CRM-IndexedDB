927fdb9843911624be87279d2ae7ba3facc0f1e6
Martin Aguilar <34758911+martin0123478@users.noreply.github.com>
2023-01-04 10:22:13 -0600

    Creando Base de Datos

---
+++ js/app.js
@@ -0,0 +1,30 @@
+(function(){
+    let DB
+     document.addEventListener('DOMContentLoaded',()=>{
+        crearDB()
+     })
+
+     function crearDB(){
+        const crearDB = window.indexedDB.open('crm',1)
+        crearDB.onerror = function(){
+            console.log('Hubo un error')
+        }
+
+        crearDB.onsuccess = function(){
+            DB = crearDB.result
+        }
+
+        crearDB.onupgradeneeded = function(e){
+            const db = e.target.result
+
+            const objectStore = db.createObjectStore('crm',{keyPath:'id',autoIncrement:true})
+            objectStore.createIndex('nombre','nombre',{unique:false});
+            objectStore.createIndex('email','email',{unique:true});
+            objectStore.createIndex('telefono','telefono',{unique:false});
+            objectStore.createIndex('empresa','empresa',{unique:false});
+            objectStore.createIndex('id','id',{unique:true});
+            console.log('lista y creada')
+
+        }
+     }
+})()

774c8e53aa3c1049f44acdb070fbc9cbe88cb65f
Martin Aguilar <34758911+martin0123478@users.noreply.github.com>
2023-01-04 15:19:40 -0600

    Editar clientes

---
+++ js/editarcliente.js
@@ -0,0 +1,93 @@
+(function(){
+    let DB
+    let idCliente
+    const nombreInput = document.querySelector('#nombre') 
+    const emailInput = document.querySelector('#email') 
+    const telefonoInput = document.querySelector('#telefono') 
+    const empresaInput = document.querySelector('#empresa') 
+    const formulario = document.querySelector('#formulario')
+    document.addEventListener('DOMContentLoaded',()=>{
+        conectarDB()
+        //Actualizar el registro
+        formulario.addEventListener('submit',actualizarCliente)
+        //Verificar el id de la url
+        const parametrosURL = new URLSearchParams(window.location.search)
+         idCliente = parametrosURL.get('id')
+        if(idCliente){
+            setTimeout(() => {
+            obtenerCliente(idCliente)  
+            }, 1000);
+           
+        }
+    })
+
+    function actualizarCliente(e){
+        e.preventDefault()
+
+        if(nombreInput.value === '' ||  emailInput.value==='' || emailInput.value==='' || empresaInput.value===''){
+            imprimirAlerta('Todos los campos son obligatorios','error')
+            return
+        }
+        //Actualizar cliente
+        const cienteActualizado = {
+            nombre: nombreInput.value,
+            email:emailInput.vaue,
+            empresa:empresaInput.value,
+            telefono: telefonoInput.value,
+            id:Number(idCliente)
+        }
+       const transaction = DB.transaction(['crm'],'readwrite')
+       const objectStore = transaction.objectStore('crm')
+
+       objectStore.put(cienteActualizado)
+       transaction.oncomplete = function(){
+        imprimirAlerta('editado correctamente')
+
+        setTimeout(() => {
+            window.location.href = 'index.html'
+        }, 3000);
+       }
+
+       transaction.onerror = function(){
+        imprimirAlerta('Hubo un error', 'error')
+       }
+    }
+
+    function obtenerCliente(id){
+        const transaction = DB.transaction(['crm'],'readwrite');
+        const objectStore = transaction.objectStore('crm');
+       const cliente = objectStore.openCursor()
+       cliente.onsuccess = function(e){
+        const cursor = e.target.result
+
+        if(cursor){
+            if(cursor.value.id === Number(id)){
+                llenarFormulario(cursor.value)
+            }
+            cursor.continue()
+        }
+       }
+    }
+
+
+    function llenarFormulario(datosCliente){
+        const {nombre,email,telefono,empresa} = datosCliente
+
+        nombreInput.value = nombre
+        emailInput.value = email
+        telefonoInput.value = telefono
+        empresaInput.value = empresa
+    }
+
+    function conectarDB(){
+        const abrirConexion  = window.indexedDB.open('crm',1)
+
+        abrirConexion.error = function(){
+            console.log('Hubo un errror')
+        }
+
+        abrirConexion.onsuccess = function(){
+            DB = abrirConexion.result
+        }
+    }
+})()
--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -7,17 +7,7 @@
         formulario.addEventListener('submit',validarCliente)
     })
 
-    function conectarDB(){
-        const abrirConexion  = window.indexedDB.open('crm',1)
-
-        abrirConexion.error = function(){
-            console.log('Hubo un errror')
-        }
-
-        abrirConexion.onsuccess = function(){
-            DB = abrirConexion.result
-        }
-    }
+    
 
     function validarCliente(e){
         e.preventDefault();
@@ -61,26 +51,6 @@
         }
     }
 
-    function imprimirAlerta(mensaje,tipo){
-    const alerta = document.querySelector('.alerta')
-    if(!alerta){
-        const divMensaje = document.createElement('div')
-        divMensaje.classList.add('px-4','py-3','rounded','max-w-lg','mx-auto','mt-6','text-center','border','alerta')
-
-        if(tipo === 'error'){
-            divMensaje.classList.add('bg-red-100','border-red-400','text-red-700')
-        }else{
-            divMensaje.classList.add('gb-green-100','border-green-400','text-green-700')
-        }   
-
-        divMensaje.textContent = mensaje
-
-        formulario.appendChild(divMensaje)
-
-        setTimeout(() => {
-            divMensaje.remove()
-        }, 3000);
-    }
-    }
+    
         
 })()
